c910b9f2c196987cf65094be2b7162a5b79728cd
karyssa-dandrea <karyssadandrea@gmail.com>
2021-11-02 10:41:41 -0700

    added event data

--- data/events.js
+++ data/events.js
@@ -79,4 +79,91 @@ export const events = [{
     date: '2021-11-14',
     link: 'https://event.etix.com/ticket/online/performanceSale.do?performance_id=8119639&method=restoreToken'
 
+}, {
+    venue: 'Crystal Ballroom',
+    opener: 'Reignwolf',
+    headliner: 'Rival Sons',
+    vax: true,
+    price: '$30',
+    image: 'https://maytherockbewithyou.com/mtrbwy/wp-content/uploads/2012/03/press_shot_1sml.jpg',
+    doors: '6:30 pm',
+    date: '2021-11-9',
+    link: 'https://event.etix.com/ticket/online/performanceSale.do?performance_id=6131266&method=restoreToken#_ga=2.106035203.1451310243.1635811477-1264417832.1635811477'
+}, {
+    venue: 'Crystal Ballroom',
+    opener: 'N/A',
+    headliner: 'Dawes',
+    vax: true,
+    price: '$28',
+    image: 'https://www.crystalballroompdx.com/system/uploads/MadborgAssets/_v/Dawes2021.jpg/header.jpg',
+    doors: '6:30 pm',
+    date: '2021-11-19',
+    link: 'https://event.etix.com/ticket/online/performanceSale.do?performance_id=8178193&method=restoreToken#_ga=2.79287060.1451310243.1635811477-1264417832.1635811477'
+}, {
+    venue: 'Star Theatre',
+    opener: 'N/A',
+    headliner: 'Black Lodge Burlesque',
+    vax: true,
+    price: '$20',
+    image: 'https://i.ticketweb.com/i/00/10/44/02/29_Edp.jpg?v=4',
+    doors: '8:00 pm',
+    date: '2021-11-18',
+    link: 'https://www.ticketweb.com/event/black-lodge-burlesque-star-theater-tickets/11351795?pl=star&REFID=clientsitewp'
+}, {
+    venue: 'Star Theatre',
+    opener: 'N/A',
+    headliner: 'Phony People',
+    vax: true,
+    price: '$20',
+    image: 'https://i.ticketweb.com/i/00/10/34/80/79_Edp.jpg?v=11',
+    doors: '7:00pm',
+    date: '2021-11-9',
+    link: 'https://www.ticketweb.com/event/phony-ppl-star-theater-tickets/11241165?pl=star&REFID=clientsitewp'
+}, {
+    venue: 'Keller Auditorium',
+    opener: 'N/A',
+    headliner: 'Mean Girls - The Musical',
+    vax: true,
+    price: '$196',
+    image: 'https://portland.broadway.com/wp-content/uploads/2018/12/012_1920_NewTourShowKeyArt_MeanGirls-671x1065.jpg',
+    doors: '7:30pm',
+    date: '2021-11-8',
+    link: 'https://www.ticketsonsale.com/tickets/mean-girls-the-musical-keller-auditorium-portland-11-10-2021-3556199'
+}, {
+    venue: 'Keller Auditorium',
+    opener: 'N/A',
+    headliner: 'Mean Girls - The Musical',
+    vax: true,
+    price: '$196',
+    image: 'https://portland.broadway.com/wp-content/uploads/2018/12/012_1920_NewTourShowKeyArt_MeanGirls-671x1065.jpg',
+    doors: '7:30pm',
+    date: '2021-11-14',
+    link: 'https://www.ticketsonsale.com/tickets/mean-girls-the-musical-keller-auditorium-portland-11-10-2021-3556199'
+}, {
+    venue: 'Bossanova Ballroom',
+    opener: 'N/A',
+    headliner: 'Dean Delray',
+    vax: true,
+    price: '$12',
+    image: 'https://static.wixstatic.com/media/a478af_3b23c185342b46a8bd89a933a8f49679f000.jpg/v1/fill/w_320,h_694,al_t,q_80,usm_0.33_1.00_0.00/a478af_3b23c185342b46a8bd89a933a8f49679f000.webp',
+    doors: '8:00pm',
+    date: '2021-11-12',
+    link: 'https://bossanovaballroom.com/events'
+}, {
+    venue: 'Bossanova Ballroom',
+    opener: 'Defiance',
+    headliner: 'The Casualties',
+    vax: true,
+    price: '$20',
+    image: 'https://s3.amazonaws.com/bit-photos/large/8413576.jpeg',
+    doors: '8:00pm',
+    date: '2021-11-14',
+    link: 'https://bossanovaballroom.com/events'
+
+
+
+
+
+
+
 }];
