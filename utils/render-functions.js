1b955fbe53dfb31c9d7f8ed8ddba6661a638848c
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-11-02 11:24:19 -0700

    started writing render function

--- utils/render-functions.js
+++ utils/render-functions.js
@@ -1,29 +1,34 @@
 export function renderCalender(events) {
-
+    const calTable = document.createElement('table');
+    const calHead = document.createElement('thead');
     
 
 
+}
+
+export function renderEvents(){
     const eventName = document.createElement('h1');
     eventName.textContent = events.headliner;
-
+    
     const eventImage = document.createElement('img');
     eventImage.src = events.image;
-
+    
     const eventOpener = document.createElement('h2');
     eventOpener.textContent = events.opener;
-
+    
     const eventDoors = document.createElement('');
     eventDoors.textContent = events.doors;
-
+    
     const eventPrice = document.createElement('');
     eventPrice.textContent = events.price;
-
+    
     const eventDate = document.createElement('');
     eventDate.textContent = events.date;
-
+    
     const eventLink = document.createElement('');
     eventLink.textContent = events.link;
-
+    
     const eventVenue = document.createElement('');
     eventVenue.textContent = events.venue;
+
 }
